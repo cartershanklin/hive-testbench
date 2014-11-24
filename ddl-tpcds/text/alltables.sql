7716278c67b10a613380886db1b5c7d7de7fb581
Zhengyang Liu <zhengyang.liu@intel.com>
2014-11-25 03:43:41 +0800

    correct malformed tabs

--- ddl-tpcds/text/alltables.sql
+++ ddl-tpcds/text/alltables.sql
@@ -279,10 +279,10 @@ drop table if exists inventory;
 
 create external table inventory
 (
-    inv_date_sk			int,
-    inv_item_sk			int,
-    inv_warehouse_sk		int,
-    inv_quantity_on_hand	int
+    inv_date_sk                 int,
+    inv_item_sk                 int,
+    inv_warehouse_sk            int,
+    inv_quantity_on_hand        int
 )
 row format delimited fields terminated by '|' 
 location '${LOCATION}/inventory';
--- ddl-tpcds/text/inventory.sql
+++ ddl-tpcds/text/inventory.sql
@@ -5,10 +5,10 @@ drop table if exists inventory;
 
 create external table inventory
 (
-    inv_date_sk			int,
-    inv_item_sk			int,
-    inv_warehouse_sk		int,
-    inv_quantity_on_hand	int
+    inv_date_sk                 int,
+    inv_item_sk                 int,
+    inv_warehouse_sk            int,
+    inv_quantity_on_hand        int
 )
 row format delimited fields terminated by '|' 
 location '${LOCATION}';
